25f6328b472a46fa5b72a0209df843464bdd77cf
husanbek <husanbek4030m@gmail.com>
2022-09-10 16:24:13 +0500

    Initial Commit

---
+++ HKeychain/HKeychainApp.swift
@@ -0,0 +1,20 @@
+//
+//  HKeychainApp.swift
+//  HKeychain
+//
+//  Created by Karavella on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HKeychainApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
